1cd2f5b0ff4fb02ec40787208820d6adc06099b1
dbobb220 <david.bobb.220@gmail.com>
2019-10-05 14:56:06 -0500

    delete src, change public

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reducer from "./reducers/index";
-import state from "./state";
-
-export default createStore(reducer, state, applyMiddleware(thunk));
