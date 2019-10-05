0e724f81252d20433c53839182dd63da675fefa2
dbobb220 <david.bobb.220@gmail.com>
2019-10-05 18:19:45 -0500

    minor change

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ app.get("/ping", function(req, res) {
   return res.send("pong");
 });
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 
 app.listen(process.env.PORT || port, () =>
   console.log(`Server listening on port ${port}!`)
