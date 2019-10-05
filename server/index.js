b8cc0fc03315247660e7ad3d3753e8b7c80c86f2
dbobb220 <david.bobb.220@gmail.com>
2019-10-05 17:06:23 -0500

    change

--- server/index.js
+++ server/index.js
@@ -1,50 +1,54 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 const port = 8080;
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const path = require('path');
-const authMiddleWare = require('./authentication').authentication;
-const tasksRoutes = require('./tasks/TaskRoutes');
-const usersRoutes = require('./users/UserRoutes');
-const sessionRoutes = require('./session/SessionRoute');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+const authMiddleWare = require("./authentication").authentication;
+const tasksRoutes = require("./tasks/TaskRoutes");
+const usersRoutes = require("./users/UserRoutes");
+const sessionRoutes = require("../session/SessionRoute");
 
 dotenv.config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@aca-practice-bx4sb.mongodb.net/${process.env.DB_DB}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@aca-practice-bx4sb.mongodb.net/${process.env.DB_DB}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
 const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   console.log(`Database connected`);
 });
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Exprees will serve up production assets
-  app.use(express.static(path.join(__dirname, "kanban/build")));
-  
+  app.use(express.static(path.join(__dirname, "public")));
+
   // Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, "kanban/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public"));
   });
-  } else{
-  app.use(express.static(path.join(__dirname, "kanban/build")));
-  }
+} else {
+  app.use(express.static(path.join(__dirname, "public")));
+}
 
 app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(usersRoutes);
 app.use(sessionRoutes);
 app.use(tasksRoutes);
 
-app.get('/ping', function (req, res) {
- return res.send('pong');
+app.get("/ping", function(req, res) {
+  return res.send("pong");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(process.env.PORT || port, () => console.log(`Server listening on port ${port}!`));
- 
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server listening on port ${port}!`)
+);
--- src/redux/reducers/editTask.js
+++
@@ -1,8 +0,0 @@
-import {EDIT_CLICK} from '../actionTypes'
-
-export const editTask = (state = [], action) => {
-    switch(action.type) {
-        case EDIT_CLICK: return action.editTask;
-        default: return state;
-    };
-}
